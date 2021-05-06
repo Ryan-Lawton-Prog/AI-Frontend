411bd385d5717987e2a16ce0a867c57795ee6213
Ryan-Lawton-Prog <50411373+Ryan-Lawton-Prog@users.noreply.github.com>
2021-05-06 16:25:01 +1000

    Updated Login to use react compenents + bootstrap

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,26 +1,28 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import React from "react";
 //import axios from 'axios';
 import "./Login.css";
-import { useHistory } from 'react-router-dom';
+import { withRouter } from 'react-router-dom'
 
-export default function Login() {
-  const [usernameV, setEmail] = useState("");
-  const [passwordV, setPassword] = useState("");
-  const history = useHistory();
-  const handleClick = () => history.push('/createAccount');
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        errorMessage: false,
+        username: '',
+        password: '',
+        validForm: false
+    };
 
-  function validateForm() {
-    return usernameV.length > 0 && passwordV.length > 0;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  function handleSubmit(event) {
+  handleSubmit(event) {
     console.log(event);
     const url = "/login";
     const postBody = {
-      username: usernameV,
-      password: passwordV
+      username: this.state.username,
+      password: this.state.password
     };
     const requestMetadata = {
       method: 'POST',
@@ -44,35 +46,59 @@ export default function Login() {
     event.preventDefault();
   }
 
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="username">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            autoFocus
-            type="username"
-            value={usernameV}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={passwordV}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-        
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    this.setState({
+      [name]: target.value
+    });
+    if (this.state.username && this.state.username.length > 0 && 
+        this.state.password && this.state.password.length > 0){
+        this.setState({validForm: true});
+    }else{
+        this.setState({validForm: false});
+    }
+  }
 
-        <button type="button" onClick={handleClick}>
-          Create Account
-        </button>
-      </Form>
-    </div>
-  );
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Username
+              <input
+                autoFocus
+                name="username"
+                type="text"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+
+          <div className="form-group">
+            <label>Password
+              <input
+                name="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+
+          <button className="btn btn-primary btn-block" type="submit" disabled={!this.state.validForm}>
+            Login
+          </button>
+          
+          <button className="btn btn-primary btn-block" type="button" onClick={() => this.props.history.push('/createAccount')}>
+            Create Account
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
+
+
+export default withRouter(Login)
