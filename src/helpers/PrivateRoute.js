f54de83138fdd1b0c111fdfec7529e7c41a453f0
Ryan-Lawton-Prog <ryan.lawton.prog@gmail.com>
2021-05-07 01:26:42 +1000

    Login/Dashboard route created

--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -1,31 +1,46 @@
 import React from "react";
-import { Route, Redirect } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
-function PrivateRoute ({component: Component, authed, ...rest}) {
-  return (
-    <Route
-    render={(props) => 
-        fetch("/auth_user", {
-          method: 'POST',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({'token':authed})}
-          )
-        .then(res => res.json())
-        .then(
-            (result) => {
-                console.log(result);
-                <Component/>
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-                <Redirect to={{pathname: '/login', state: {from: props.location}}} />
-            }
-        )
-    }
-    />
-  )
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.component = props.component;
+    this.state = {
+        loading: true
+    };
+  }
+
+  componentDidMount() {
+    fetch("/auth_user", {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({'token':localStorage.getItem('user')})}
+      )
+    .then(res => res.json())
+    .then(
+        (result) => {
+            console.log(result);
+            this.setState({loading: false})
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+            console.log(error)
+            this.props.history.push('/login');
+        }
+    )
+  }
+
+  render() {
+    return (
+      <div className="PrivateRoute">
+        {this.state.loading && <h1>Loading</h1>}
+        {!this.state.loading && <this.component/>}
+      </div>
+    );
+  }
 }
 
-export default PrivateRoute
+
+export default withRouter(Login)
