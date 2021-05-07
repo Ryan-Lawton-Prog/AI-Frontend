e7e0d92439d50f67bae96291e87f13d244040025
Ryan-Lawton-Prog <ryan.lawton.prog@gmail.com>
2021-05-08 01:17:02 +1000

    Constructed all the components

---
+++ src/containers/Root/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import dorohedoro from '../../assets/dorohedoro.jpeg';
+
+export default class Header extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="Header">
+        <header class="masthead" style={{backgroundImage: `url(${dorohedoro})`}}>
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-10 mx-auto">
+                        <div class="site-heading">
+                            <h1>Clean Blog</h1>
+                            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+      </div>
+    );
+  }
+}
