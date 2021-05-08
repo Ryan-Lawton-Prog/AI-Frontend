6e1ec229c3bdecfdf456fc100a662229452e6c43
Ryan-Lawton-Prog <ryan.lawton.prog@gmail.com>
2021-05-08 15:39:36 +1000

    Completed blog iteration

--- src/containers/Root/Body.js
+++ src/containers/Root/Body.js
@@ -4,7 +4,48 @@ export default class Body extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+        loading: true,
+        blogs: [{
+            'author':'Ryan Lawton',
+            'title': 'Default blog',
+            'subtitle': 'why, just why',
+            'date': 12341294,
+            'time': 3,
+            '_id': 1
+        }]
     };
+  }
+
+  componentDidMount() {
+    fetch("/blog/get_blogs", {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+    .then(
+        (result) => {
+            console.log(result);
+            var blogs = [];
+            for (var key in result) {
+                result[key]['_id'] = key;
+                blogs.push(result[key]);
+            }
+
+            blogs.sort((a, b) => a.time - b.time);
+
+            this.setState(state => ({
+                loading: false,
+                blogs: [...state.blogs, ...blogs]
+            }))
+            console.log(this.state.blogs);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+            console.log(error)
+        }
+    )
   }
 
   render() {
@@ -13,51 +54,26 @@ export default class Body extends React.Component {
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-md-10 mx-auto">
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Ma must explore, and this is exploration at its greatest</h2>
-                            <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 24, 2021
-                        </p>
-                    </div>
-                    <hr />
-                    <div class="post-preview">
-                        <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 18, 2021
-                        </p>
-                    </div>
-                    <hr />
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
-                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on August 24, 2021
-                        </p>
-                    </div>
-                    <hr />
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Failure is not an option</h2>
-                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on July 8, 2021
-                        </p>
-                    </div>
-                    <hr />
+                    {this.state.loading && <h1>Loading</h1>}
+
+                    {!this.state.loading && this.state.blogs.map(blog => {
+                        return (
+                            <div key={blog._id}>
+                                <div class="post-preview">
+                                    <a href={`blog/`+blog._id}>
+                                        <h2 class="post-title">{blog.title}</h2>
+                                        <h3 class="post-subtitle">{blog.subtitle}</h3>
+                                    </a>
+                                    <p class="post-meta">
+                                        Posted by 
+                                        <a href="#!">{blog.author}</a>
+                                        on {blog.date} 
+                                    </p>
+                                </div>
+                                <hr />
+                            </div>
+                        )
+                    })}
 
                     <div class="clearfix"><a class="btn btn-primary float-right" href="#!">Older Posts →</a></div>
                 </div>
